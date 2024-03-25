a48c4bc75f7263372e58a5749bb07b2f468149ff
Jeff Allen <jrallen724@gmail.com>
2024-03-25 15:03:51 -0400

    xx

---
+++ main.js
@@ -0,0 +1,24 @@
+// attach event listener to form to get user data
+
+// attach event (click) listeners to each "game box" 
+
+//initialize the game
+
+// check which game mode is being played
+
+// set win conditions
+
+// determine current player
+
+// after each move, check win conditions and if not met set other player as active
+
+// human vs human, next implement easy ai, next impossible ai
+
+const form = document.querSelector("#myForm");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    const data - Object.fromEntries(formData);
+    
+})
